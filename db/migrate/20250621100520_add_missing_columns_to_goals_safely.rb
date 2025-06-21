56f96c791b8a64c29090cc01b589a8ec285a0c5f
Naoya Iijima <meteor5963@gmail.com>
2025-06-21 19:06:22 +0900

    reset

---
+++ db/migrate/20250621100520_add_missing_columns_to_goals_safely.rb
@@ -0,0 +1,17 @@
+class AddMissingColumnsToGoalsSafely < ActiveRecord::Migration[8.0]
+  def change
+    if table_exists?(:goals)
+      unless column_exists?(:goals, :status)
+        add_column :goals, :status, :string, default: 'not_started', null: false
+      end
+
+      unless column_exists?(:goals, :progress_check)
+        add_column :goals, :progress_check, :text
+      end
+
+      unless column_exists?(:goals, :created_by_ai)
+        add_column :goals, :created_by_ai, :boolean, default: false, null: false
+      end
+    end
+  end
+end 
