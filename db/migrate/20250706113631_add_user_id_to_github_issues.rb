143db8717c2d3ebfde0da83ad337c7db3f69bd46
Naoya Iijima <meteor5963@gmail.com>
2025-07-07 00:27:57 +0900

    wip

--- db/migrate/20250706113631_add_user_id_to_github_issues.rb
+++ db/migrate/20250706113631_add_user_id_to_github_issues.rb
@@ -1,5 +1,7 @@
 class AddUserIdToGithubIssues < ActiveRecord::Migration[8.0]
   def change
-    add_column :github_issues, :user_id, :integer, null: false
+    unless column_exists?(:github_issues, :user_id)
+      add_column :github_issues, :user_id, :integer, null: false
+    end
   end
 end
